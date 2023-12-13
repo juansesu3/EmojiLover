09f588c273d1bca94142c9567009a222211318f8
juansesu3 <juanseu3@hotmail.com>
2023-12-13 11:17:37 +0100

    updated

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,18 +7,31 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+enum Emoji: String, CaseIterable  {
+    case 👻,😈,👹,👽
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    @State var selection: Emoji = .👹
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id:\.self){emoji in Text(emoji.rawValue)}
+                }
+            }
+            .navigationTitle("Emoji Lovers!")
+            .pickerStyle(.segmented)
+            .padding()
+        }
+    }
+        
+}
+
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{
+        ContentView()
+    }
 }
